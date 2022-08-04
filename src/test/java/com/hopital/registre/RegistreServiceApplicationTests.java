70e0e364a0ef9110305ce7f6cacd1dfcea1051bf
Samano CASTRE <castrosama@live.fr>
2022-08-04 12:30:47 +0200

    init du microservice registre-service (eureka)

---
+++ src/test/java/com/hopital/registre/RegistreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hopital.registre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
